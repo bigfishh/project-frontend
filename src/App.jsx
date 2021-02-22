af2385c6e0fdcda9d3acf03da36d20d73a953093
bigfishh <anniezheng.anniee@gmail.com>
2021-02-21 23:19:46 -0500

    Initial commit with create snowpack app.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default App 
