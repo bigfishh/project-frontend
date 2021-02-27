92e2f764aa337e05faaac753e9e35182219d656c
bigfishh <anniezheng.anniee@gmail.com>
2021-02-26 19:28:07 -0500

    Add login and sign up forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import Login from './Login'
+import Signup from './Signup'
 
 function App() {
 
+    const [loggedIn, setLoggedIn] = useState(false)
+
+    useEffect(function() {
+        fetch("https://test.anniezheng.dev/")
+        .then(resp => resp.json())
+        .then(console.log)
+    }, [])
+
+    function handleLogout() {
+        localStorage.clear()
+        setLoggedIn(false)
+    }
+
+    function renderForms() {
+        if (loggedIn) {
+            return <button onClick={handleLogout}>Log out</button>
+        } else {
+            return (
+                <div>
+                    <Login setLoggedIn={setLoggedIn}/>
+                    <Signup setLoggedIn={setLoggedIn}/>
+                </div>
+            )
+        }
+    }
+
     return(
         <div>
-            <h1>Hello World</h1>
+            {renderForms()}
         </div>
     )
 }
---
+++ src/Login.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+import qs from 'query-string'
+
+function Login({setLoggedIn}) {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch("https://test.anniezheng.dev/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: qs.stringify({
+                username: username,
+                password: password
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.accessToken) {
+                localStorage.setItem("token", data.accessToken)
+                setLoggedIn(true)
+            } else {
+                console.log(data.message)
+            }
+        });
+    };
+
+    return(
+        <div>
+            Login Form
+            <form onSubmit={handleSubmit}>
+                <input placeholder="username" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <input placeholder="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button type="submit" value="Submit">Log in</button>
+            </form>
+        </div>
+    )
+};
+
+export default Login;
